957f8022b05f085bde18521cfd4a9ae54ff03aa9
VladislavVik1 <Dakraman1232@ukr.net>
2025-06-11 15:30:35 +0300

    1

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from 'react-redux';
-import { deleteContact } from '../../redux/contacts/operations';
+import { deleteContact } from '../../redux/contacts/contactsOps';
 import styles from './Contact.module.css';
 
 export default function Contact({ contact }) {
