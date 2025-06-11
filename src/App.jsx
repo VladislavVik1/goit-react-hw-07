31360ae200622a4c56a31107c811f13e7e2e5f13
VladislavVik1 <Dakraman1232@ukr.net>
2025-06-11 15:18:15 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from './redux/contacts/operations';
-import { selectLoading, selectError } from './redux/contactsSlice';
+import { selectLoading, selectError } from './redux/contacts/contactsSlice';
 import ContactForm from './components/ContactForm/ContactForm';
 import ContactList from './components/ContactList/ContactList';
 import SearchBox from './components/SearchBox/SearchBox';
